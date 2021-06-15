163b45de476b968d591aa00f856349a04f769484
pranav21999 <84624284+pranav21999@users.noreply.github.com>
2021-06-15 22:22:51 +0530

    Update employee is present or absent

--- empwage.java
+++ empwage.java
@@ -3,6 +3,14 @@ class empwage
 	public static void main(String args[])
 
 	{
-	System.out.println("Welcome to empwage computation")
+	 System.out.println("Welcome to empwage computation")
+		int fulltime=1;
+		double value= (Math.floor(Math.random() * 10) % 2);
+
+	  if(value == fulltime)
+		System.out.println("employee is present");
+	  else
+		System.out.println("employee is absent");
+
 	}
 }
