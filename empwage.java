387d264a6362f8d90d38e8c43c8baf9de766ac28
pranav21999 <84624284+pranav21999@users.noreply.github.com>
2021-06-15 23:03:10 +0530

    Update daily wages for full time employee

--- empwage.java
+++ empwage.java
@@ -3,14 +3,21 @@ class empwage
 	public static void main(String args[])
 
 	{
-	 System.out.println("Welcome to empwage computation")
-		int fulltime=1;
-		double value= (Math.floor(Math.random() * 10) % 2);
+	 System.out.println("Welcome to empwage computation");
+	
+		int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
 
-	  if(value == fulltime)
-		System.out.println("employee is present");
-	  else
-		System.out.println("employee is absent");
+        int empHrs = 0;
+        int empwage = 0;
 
+        double empcheck = Math.floor(Math.random() * 10) % 2; // generate random no 0 or 1 
+
+        if (empcheck == IS_FULL_TIME) // condition to check emp is present or not
+                empHrs = 8;
+        else
+                empHrs = 0;
+        empwage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage : " + empwage);
 	}
 }
