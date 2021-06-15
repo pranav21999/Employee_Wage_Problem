2d3ac5bc94cbaf827544d6375e60a3b9fa6e2dc5
pranav21999 <84624284+pranav21999@users.noreply.github.com>
2021-06-15 23:17:49 +0530

    Update part time employee wage

--- empwage.java
+++ empwage.java
@@ -6,18 +6,31 @@ class empwage
 	 System.out.println("Welcome to empwage computation");
 	
 		int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
+        int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
 
         int empHrs = 0;
         int empwage = 0;
 
-        double empcheck = Math.floor(Math.random() * 10) % 2; // generate random no 0 or 1 
+        double empcheck = Math.floor(Math.random() * 10) % 3; // generate random no 0 or 1 or 2 
 
-        if (empcheck == IS_FULL_TIME) // condition to check emp is present or not
+        if (empcheck == IS_FULL_TIME)//comdition to check full time
+		{
                 empHrs = 8;
-        else
+				System.out.println("Employee is present full time");
+	    }
+		else if (empcheck == IS_PART_TIME)//comdition to check part time
+		{
+			
+                empHrs = 4;
+				System.out.println("Employee is present part time");
+	    }
+		else
+		{	
+				System.out.println("Employee is absent");
                 empHrs = 0;
-        empwage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage : " + empwage);
+		}
+				empwage = empHrs * EMP_RATE_PER_HOUR;//calculate empwage 
+				System.out.println("Employee Wage : " + empwage);
 	}
 }
