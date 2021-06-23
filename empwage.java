a0c25077365f13e00cb57f1e1f9683ac03d270d9
pranav21999 <84624284+pranav21999@users.noreply.github.com>
2021-06-23 19:51:33 +0530

    method to calculate total Wage by Company is added

--- empwage.java
+++ empwage.java
@@ -1,16 +1,17 @@
-public class EmpWage {
+public class EmpWage 
+{
 	// Instance variables
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-
 	private final String company;
 	private final int wagePerHour;
 	private final int workingDays;
 	private final int maxHoursPerMonth;
 	private int totalEmpWage;
 
-	// Constructor
-	public EmpWage(String company, int wagePerHour, int workingDays, int maxHoursPerMonth) {
+	// parameterised Constructor
+	public EmpWage(String company, int wagePerHour, int workingDays, int maxHoursPerMonth) 
+	{
 		this.company = company;
 		this.wagePerHour = wagePerHour;
 		this.workingDays = workingDays;
@@ -36,24 +37,20 @@ public class EmpWage {
 				empHrs = 0;
 			}
 			totalEmpHrs += empHrs;
-			// System.out.println("Day : " + totalWorkingDays + " Emp Hr : " + empHrs);
+			 System.out.println("Day : " + totalWorkingDays + " Emp Hr : " + empHrs);
 		}
 		totalEmpWage = totalEmpHrs * wagePerHour;
-		System.out.println ("Total Emp Wage for company " + company + " is " + totalEmpWage);
 	
 	}
-
 	
-
-	
-
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
+		//save the total wages for each company
 		EmpWage  WIPRO= new EmpWage("WIPRO", 20, 20, 100);
 		EmpWage  TCS= new EmpWage("TCS", 10, 40, 200);
-		WIPRO.computeWage(); // Compute for dMart
-		//System.out.println(WIPRO);
-		TCS.computeWage(); // Compute for reliance
-		//System.out.println(TCS);
+		WIPRO.computeWage(); // Compute for WIPRO/
+		System.out.println("Total wage for wipro"+WIPRO);
+		TCS.computeWage();// Compute for TCS
+		System.out.println("Total wage for TCS"+TCS);
 	}
-}
+}		
