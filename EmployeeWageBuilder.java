759c321a19f0cd14e3a92b182973baf23115bac1
pranav21999 <pranavdani99@gmail.com>
2021-06-28 02:00:50 +0530

    Update feature to calculate Daily wage along with Total wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -7,7 +7,7 @@ public class EmployeeWageBuilder implements EmpWageInterface {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
-	//creating ArratList
+
 	private ArrayList<CompanyEmpWage> companyEmpArrayList;
 	
 
@@ -16,14 +16,16 @@ public class EmployeeWageBuilder implements EmpWageInterface {
 
 		System.out.println("Welcome to Employee Wage calculation");
 		EmployeeWageBuilder empwage = new EmployeeWageBuilder();
+		
 		empwage.addCompanyEmpWage(" Dmart ", 20, 2, 10);//passing arguments to the method
+	
 		System.out.println("\n");
 		
 		empwage.addCompanyEmpWage(" Reliance ", 5, 25, 25);////passing arguments to the method
 		empwage.computeEmpWage();
 	}
 
-   	//constructor
+   //constructor
 	public EmployeeWageBuilder() {
 		companyEmpArrayList = new ArrayList<CompanyEmpWage>();
 	}
@@ -35,28 +37,32 @@ public class EmployeeWageBuilder implements EmpWageInterface {
 				maxHoursPerMonth);
 		companyEmpArrayList.add(companyEmpWage);
 	}
-
- 	public void computeEmpWage() {
+	
+	
+	public void computeEmpWage() {
 		for (int i = 0; i < companyEmpArrayList.size(); i++) {
-			CompanyEmpWage companyEmpwage = companyEmpArrayList.get(i);
+			CompanyEmpWage companyEmpwage = companyEmpArrayList.get(i);			
 			companyEmpwage.setTotalEmpWage(this.computeEmpWage(companyEmpwage));
+			System.out.println("Company Name: " + companyEmpwage.company);
 			System.out.println(companyEmpwage.company + "Total Wage is " + companyEmpwage.totalWage);
 		}
-	}
-
+	}	
+	
  	//method to compute Total employeeWage
 	public int computeEmpWage(CompanyEmpWage companyEmpWage) {
 		// variables
-		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		int empHrs, totalEmpHrs = 0, totalWorkingDays = 0, day=0;
+		//array to  calculate daily wage
+		int daily_wage_array[] = new int[companyEmpWage.numOfWorkingDays];
+		
 		// computation
 		while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
-
 			totalWorkingDays++;
 			//Random function to check Employee is present(partTime or fullTime) or absent
 			int empCheck = (int) (Math.random() * 10) % 3;
+		
 			// using switch statement to check the employee working time
 			switch (empCheck) {
-
 			case IS_FULL_TIME:
 				empHrs = 8;
 				System.out.println("Employee is present as Full time");
@@ -71,9 +77,16 @@ public class EmployeeWageBuilder implements EmpWageInterface {
 				System.out.println("Employee is Absent");
 			}
 			totalEmpHrs += empHrs;
+			 daily_wage_array[day] = empHrs * companyEmpWage.empRatePerHour;	
+			 day++;
 			System.out.println("Day#" + totalWorkingDays + "Emp Hr:" + empHrs);
 			
 		}
+		 for (int j = 0; j < companyEmpWage.numOfWorkingDays; j++) {
+	             day = j + 1;
+	            System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
+	           
+	        }		
 		// calculating total Employee wage for company
 		return totalEmpHrs * companyEmpWage.empRatePerHour;
 
